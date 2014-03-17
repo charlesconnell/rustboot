72409000005c38b371a624b4a6b949dd7e9e6c1b
Piotr Czarnecki <pioczarn@gmail.com>
2014-03-17 13:15:03 +0100

    common reg macros

--- arch/common/x86/reg.rs
+++ arch/common/x86/reg.rs
@@ -18,63 +18,58 @@ impl Eflags {
     }
 }
 
-define_flags!(CR0Flags: uint {
+macro_rules! impl_reg(
+    ($name:ident ( $reg:expr ): $T:ident) => (
+        pub struct $name;
+
+        impl $name {
+            #[inline]
+            pub fn read() -> $T {
+                unsafe {
+                    let flags;
+                    asm!(concat!("mov $0, ", $reg) : "=r"(flags) ::: "intel");
+                    $T(flags)
+                }
+            }
+
+            #[inline]
+            pub fn write(f: $T) {
+                match f {
+                    $T(val) => unsafe {
+                        asm!(concat!("mov ", $reg, ", $0") :: "r"(val) :: "volatile", "intel");
+                    }
+                }
+            }
+        }
+    );
+
+    (
+        $name:ident ( $reg:expr ), $flags:ident: $T:ty {
+            $($flag:ident $(= $v:expr)*),*
+        }
+    ) => (
+        define_flags!($flags: $T { $( $flag $(= $v)* ),* })
+
+        impl_reg!($name($reg): $flags)
+
+        impl core::ops::BitOr<$flags, $flags> for CR0 {
+            #[inline(always)]
+            fn bitor(&self, other: &$flags) -> $flags {
+                match ($name::read(), other) {
+                    ($flags(lhsf), &$flags(rhsf)) => $flags(lhsf | rhsf)
+                }
+            }
+        }
+    )
+)
+
+impl_reg!(CR0("cr0"), CR0Flags: uint {
     CR0_PG = 1 << 31
 })
 
-pub struct CR0;
-
+impl_reg!(CR2("cr2"): Page)
 // http://www.jaist.ac.jp/iscenter-new/mpc/altix/altixdata/opt/intel/vtune/doc/users_guide/mergedProjects/analyzer_ec/mergedProjects/reference_olh/mergedProjects/instructions/instruct32_hh/vc178.htm
-impl CR0 {
-    #[inline]
-    pub fn read() -> CR0Flags {
-        unsafe {
-            let flags;
-            asm!("mov $0, cr0" : "=r"(flags) ::: "intel");
-            CR0Flags(flags)
-        }
-    }
-
-    #[inline]
-    pub fn write(f: CR0Flags) {
-        match f {
-            CR0Flags(val) => unsafe {
-                asm!("mov cr0, $0" :: "r"(val) :: "volatile", "intel");
-            }
-        }
-    }
-}
-
-impl core::ops::BitOr<CR0Flags, CR0Flags> for CR0 {
-    #[inline(always)]
-    fn bitor(&self, other: &CR0Flags) -> CR0Flags {
-        match (CR0::read(), other) {
-            (CR0Flags(flags1), &CR0Flags(flags2)) => CR0Flags(flags1 | flags2)
-        }
-    }
-}
-
-pub struct CR3;
-
-// http://www.jaist.ac.jp/iscenter-new/mpc/altix/altixdata/opt/intel/vtune/doc/users_guide/mergedProjects/analyzer_ec/mergedProjects/reference_olh/mergedProjects/instructions/instruct32_hh/vc178.htm
-impl CR3 {
-    #[inline]
-    pub fn read() -> Page {
-        unsafe {
-            let ptr: uint;
-            asm!("mov $0, cr3" : "=r"(ptr) ::: "intel");
-            transmute(ptr)
-        }
-    }
-
-    #[inline]
-    pub fn write(ptr: Page) {
-        unsafe {
-            let ptr: uint = transmute(ptr);
-            asm!("mov cr3, $0" :: "r"(ptr) :: "volatile", "intel");
-        }
-    }
-}
+impl_reg!(CR3("cr3"): Page)
 
 // Any of descriptor table (IDT, GDT) registers
 #[packed]
