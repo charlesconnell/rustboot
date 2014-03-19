b607c40790c32c07047362cd1c23f33decbc7c03
Piotr Czarnecki <pioczarn@gmail.com>
2014-03-19 17:49:01 +0100

    process: add mod.
    Use uints for ptrs
    
    Process use uint
    
    arm process with mmu
    
    FIX kernel process
    
    process store dir physical
    
    kernel process: mm::virtual -> mm
    
    elf HeaderFlags
    
    elf fix flags
    
    add diff comment
    
    elf TODO: fix loading
    
    fix elf mod after rebase
    
    elf work
    
    elf wrong merge
    
    fix define_flags elf
    
    elf fix merge
    
    elf32 don't use USER flag
    
    elf fix import
    
    fix elf loading
    
    elf cleanup
    
    elf cleanup 2
    
    elf32 revert delete?
    
    mm revert map
    
    x86 mmu fix directory
    
    USER: mmu map both
    
    mmu improve clone
    
    kernel::process: pub vis

--- kernel/elf/mod.rs
+++ kernel/elf/mod.rs
@@ -2,6 +2,7 @@ use core::ptr::{copy_nonoverlapping_memory, set_memory, offset};
 use core::mem::transmute;
 use core::option::{Option, Some, None};
 
+use kernel::process::Process;
 use kernel::mm;
 use util::ptr::mut_offset;
 
@@ -42,14 +43,49 @@ struct ELFIdent {
     ei_pad: [u8, ..7]
 }
 
-impl elf32::Elf32_Phdr {
-    unsafe fn load(&self, buffer: *u8) {
-        let vaddr = self.p_vaddr as *mut u8;
-        let mem_size = self.p_memsz as uint;
-        let file_pos = self.p_offset as int;
-        let file_size = self.p_filesz as uint;
+impl self::Ehdr {
+    pub unsafe fn spawn_process(&self) -> Process {
+        let mut task = Process::new();
+        //TODO: Verify file integrity
+        let buffer: *u8 = transmute(self);
+        let ph_size = self.e_phentsize as int;
+        let ph_base = offset(buffer, self.e_phoff as int);
 
-        mm::map(vaddr, mem_size, mm::RW | mm::USER);
+        let mut stack_flags = mm::RW;
+
+        int::range(0, self.e_phnum as uint, |i| {
+            let pheader = offset(ph_base, ph_size * i as int) as *Phdr;
+
+            match (*pheader).p_type {
+                PT_NULL => {}
+                PT_LOAD => (*pheader).load(&task, buffer),
+                PT_DYNAMIC => (*pheader).load(&task, buffer),
+                PT_GNU_STACK => {
+                    if !((*pheader).p_flags & !PT_X).is_zero() {
+                        // We don't need an executable stack
+                        stack_flags = mm::Flags::zero();
+                    }
+                },
+                _ => {}
+            }
+        });
+
+        static stack_bottom: u32 = 0xC0000000;
+        let stack_vaddr = (stack_bottom - 0x1000) as *mut u8;
+        task.mmap(stack_vaddr, 0x1000, stack_flags);
+        let stack_ptr = mut_offset(stack_bottom as *mut u8, -(((4 + 5 + 15) & !0xF) + 8 + 4 + 4 + 4));
+        let argv_ptr = stack_ptr as *mut *mut u8;
+        let envp_ptr = mut_offset(argv_ptr, 2);
+        let auxv_ptr = mut_offset(argv_ptr, 1) as *mut Auxv;
+        let str_ptr = mut_offset(stack_bottom as *mut u8, -(4 + 5));
+
+        *mut_offset(argv_ptr, 1) = transmute(0);
+        *envp_ptr = transmute(0);
+        *auxv_ptr = Auxv { a_type: AT_NULL, a_un: AuxvValue { data: 0 } };
+
+        let (strs, len): (*u8, uint) = transmute("test\0");
+        copy_nonoverlapping_memory(str_ptr, strs, len);
+        *argv_ptr = str_ptr;
 
         copy_nonoverlapping_memory(vaddr, offset(buffer, file_pos), file_size);
         set_memory(mut_offset(vaddr, file_pos + file_size as int), 0, mem_size - file_size);
@@ -63,7 +99,13 @@ impl elf64::Elf64_Phdr {
         let file_pos = self.p_offset as int;
         let file_size = self.p_filesz as uint;
 
-        mm::map(vaddr, mem_size, mm::RW | mm::USER);
+        let flags = if !(self.p_flags & PT_W).is_zero() {
+            mm::RW
+        } else {
+            mm::Flags::zero()
+        };
+
+        task.mmap(vaddr, mem_size, flags);
 
         copy_nonoverlapping_memory(vaddr, offset(buffer, file_pos), file_size);
         set_memory(mut_offset(vaddr, file_pos + file_size as int), 0, mem_size - file_size);
