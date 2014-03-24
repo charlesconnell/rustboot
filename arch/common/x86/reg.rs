354d92012e012c4d017a67e903c93be6b076ec4a
Piotr Czarnecki <pioczarn@gmail.com>
2014-03-24 18:52:44 +0100

    modify macros for common x86 reg

--- arch/common/x86/reg.rs
+++ arch/common/x86/reg.rs
@@ -18,10 +18,8 @@ impl Eflags {
     }
 }
 
-macro_rules! impl_reg(
-    ($name:ident ( $reg:expr ): $T:ident) => (
-        pub struct $name;
-
+macro_rules! impl_read_write (
+    ($name:ident : $T:ident, $reg:expr) => (
         impl $name {
             #[inline] #[allow(dead_code)]
             pub fn read() -> $T {
@@ -42,34 +40,21 @@ macro_rules! impl_reg(
             }
         }
     );
-
-    (
-        $name:ident ( $reg:expr ), $flags:ident: $T:ty {
-            $($flag:ident $(= $v:expr)*),*
-        }
-    ) => (
-        define_flags!($flags: $T { $( $flag $(= $v)* ),* })
-
-        impl_reg!($name($reg): $flags)
-
-        impl core::ops::BitOr<$flags, $flags> for $name {
-            #[inline(always)] #[allow(dead_code)]
-            fn bitor(&self, other: &$flags) -> $flags {
-                match ($name::read(), other) {
-                    ($flags(lhsf), &$flags(rhsf)) => $flags(lhsf | rhsf)
-                }
-            }
-        }
-    )
 )
 
-impl_reg!(CR0("cr0"), CR0Flags: uint {
+define_reg!(CR0, CR0Flags: uint {
     CR0_PG = 1 << 31
 })
 
-impl_reg!(CR2("cr2"): Page)
+impl_read_write!(CR0: CR0Flags, "cr0")
+
+pub struct CR2;
+impl_read_write!(CR2: Page, "cr2")
+
+pub struct CR3;
+impl_read_write!(CR3: Page, "cr3")
+
 // http://www.jaist.ac.jp/iscenter-new/mpc/altix/altixdata/opt/intel/vtune/doc/users_guide/mergedProjects/analyzer_ec/mergedProjects/reference_olh/mergedProjects/instructions/instruct32_hh/vc178.htm
-impl_reg!(CR3("cr3"): Page)
 
 // Any of descriptor table (IDT, GDT) registers
 #[packed]
