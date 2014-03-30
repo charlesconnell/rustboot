3e0f4ed0d15cb240f5388de35ae4d31d703f9dd1
Piotr Czarnecki <pioczarn@gmail.com>
2014-03-30 12:25:45 +0200

    add syscalls

--- kernel/syscall.rs
+++ kernel/syscall.rs
@@ -1,24 +1,41 @@
+use platform = arch::x86;
+use cpu::Context;
+
 pub struct args(u32, u32, u32, u32);
 
 macro_rules! syscall(
+    (fn $name:ident() -> $ret:ty $func:expr) => (
+        pub fn $name(regs: &mut Context) {
+            let ret: $ret = { $func };
+            regs.eax = ret as u32;
+        }
+    );
+    (fn $name:ident($a0:ident: $t0:ty) -> $ret:ty $func:expr) => (
+        pub fn $name(regs: &mut Context) {
+            let $a0 = regs.ebx as $t0;
+            let ret: $ret = { $func };
+            regs.eax = ret as u32;
+        }
+    );
     // 1 arg
     (fn $name:ident($a0:ident: $t0:ty) $func:expr) => (
-        fn $name(regs: &mut idt::Registers) {
+        fn $name(regs: &mut Context) {
             let $a0 = regs.ebx as $t0;
             $func
         }
     );
     // 3 args
     (fn $name:ident($a0:ident: $t0:ty, $a1:ident: $t1:ty, $a2:ident: $t2:ty) -> $ret:ty $func:expr) => (
-        fn $name(regs: &mut idt::Registers) {
+        fn $name(regs: &mut Context) {
             let $a0 = regs.ebx as $t0;
             let $a1 = regs.ecx as $t1;
             let $a2 = regs.edx as $t2;
-            regs.eax = { $func } as $ret;
+            let ret: $ret = { $func };
+            regs.eax = ret as u32;
         }
     );
     (fn $name:ident($a0:ident: $t0:ty, $a1:ident: $t1:ty, $a2:ident: $t2:ty) $func:expr) => (
-        fn $name(regs: &mut idt::Registers) {
+        fn $name(regs: &mut Context) {
             let $a0 = regs.ebx as $t0;
             let $a1 = regs.ecx as $t1;
             let $a2 = regs.edx as $t2;
@@ -27,9 +44,72 @@ macro_rules! syscall(
     );
 )
 
-pub fn handler(a: &mut args) -> u32 {
-    0
+pub fn handler(ctx: &mut Context) {
+    // let args(f, _, _, _) = *a;
+    match ctx.eax {
+        1 => {
+            exit(ctx)
+        }
+        3 => {
+            read(ctx)
+        }
+        5 => {
+            open(ctx)
+        }
+        6 => {
+            close(ctx)
+        }
+        // 12 => {
+        //     chdir(ctx)
+        // }
+        23 => {
+            setuid(ctx)
+        }
+        243 => {
+            set_thread_area(ctx)
+        }
+        // 25 => {
+        //     getuid(ctx)
+        // }
+        _ => {
+            platform::io::puti(ctx.eax as int);
+            platform::io::putc(' ' as u8);
+        }
+    }
 }
 
 pub fn init() {
 }
+
+syscall!(fn exit(error_code: int) -> uint {
+    platform::io::puti(error_code as int);
+    0
+})
+
+syscall!(fn read(fd: uint, buf: *u8, count: uint) -> uint {
+    0
+})
+
+syscall!(fn open(filename: *u8, flags: int, mode: uint) -> uint {
+    0
+})
+
+syscall!(fn close(fd: uint) -> uint {
+    0
+})
+
+syscall!(fn chdir(filename: *u8) -> uint {
+    0
+})
+
+syscall!(fn setuid(uid: uint) -> uint {
+    0
+})
+
+// syscall!(fn getuid() -> uint {
+//     0
+// })
+
+syscall!(fn set_thread_area(entry: *platform::cpu::IdtEntry) -> uint {
+    0
+})
