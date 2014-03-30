bd62a220b859d1fc5ad2248d35330c8719abd443
Piotr Czarnecki <pioczarn@gmail.com>
2014-03-30 17:52:56 +0200

    USER hello world!

--- user/hello.rs
+++ user/hello.rs
@@ -2,7 +2,18 @@
 
 extern crate core;
 
+use core::slice;
+use core::mem::transmute;
+
+extern {
+	fn write(fd: int, buffer: *u8, count: uint) -> uint;
+}
+
 #[start]
 pub fn main(_: int, _: **u8) -> int {
+	unsafe {
+		let sl: slice::Slice<u8> = transmute("Hello, world!");
+		write(0, sl.data, sl.len);
+	}
     42
 }
