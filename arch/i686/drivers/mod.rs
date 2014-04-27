d2e31ee73b3ded346bc880e0cc5016bc4e0c8f9c
Piotr Czarnecki <pioczarn@gmail.com>
2014-04-27 18:53:59 +0200

    i686: Kernel structure

--- arch/i686/drivers/mod.rs
+++ arch/i686/drivers/mod.rs
@@ -2,21 +2,21 @@ use core::option::{Option, None};
 use core::mem::transmute;
 
 use cpu::interrupt::{interrupt_handler, Isr, Int};
+use kernel::Kernel;
 use kernel;
+
 pub mod pic;
 pub mod vga;
 pub mod keyboard;
 
 pub static mut keydown: Option<fn(u8)> = None;
 
-pub fn init() {
+pub fn init(kernel: &mut Kernel) {
     vga::clear_screen(vga::LightRed);
     vga::cursor_at(0);
 
     unsafe {
-        kernel::int_table.map(|mut t| {
-            t.enable_maskable(keyboard::IRQ, keyboard::isr_addr());
-            t.set_intr_gate(0x80, Isr::new(transmute(0x80u8), false));
-        });
+        kernel.interrupts.enable_maskable(keyboard::IRQ, keyboard::isr_addr());
+        kernel.interrupts.set_intr_gate(0x80, Isr::new(transmute(0x80u8), false));
     }
 }
